3481d6101f0853f76a4fb4c5f762a9eb5f5e91ae
Noah King <noah@horizon3.ai>
2020-08-03 17:58:39 -0400

    use history

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,36 +1,32 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 function Footer() {
-    return (
-      <div>
-      <footer 
-      style ={{
-          backgroundColor:"#5B9098",
+  const history = useHistory();
+  return (
+    <div>
+      <footer
+        style={{
+          backgroundColor: "#5B9098",
           color: "white",
           padding: "30px",
           bottom: "0",
           left: "0",
           width: "100%",
-          flexShrink: "0"
+          flexShrink: "0",
         }}
-        > 
+      >
         <div className="footer-container">
-          <span 
-          id="copyright">&copy; {new Date().getFullYear()} 
-          &nbsp;COPYRIGHT </span>
-          <span 
-          id="contact-link" 
-          onClick={(event) => (window.location.href = "/contact")}>
-          CONTACT </span>
+          <span id="copyright">
+            &copy; {new Date().getFullYear()}
+            &nbsp;COPYRIGHT{" "}
+          </span>
+          <span id="contact-link" onClick={history.push("/contact")}>
+            CONTACT{" "}
+          </span>
         </div>
       </footer>
-      </div>
-    );
-  }
-  export default Footer
-
-
-
-
-
-
-
+    </div>
+  );
+}
+export default Footer;
